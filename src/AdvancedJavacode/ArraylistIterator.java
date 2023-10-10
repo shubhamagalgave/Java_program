6220b66fbb3454e60f9b88b621b42846156d4dfd
shubham <shubhamagalgave11111@gmail.com>
2023-10-10 19:53:25 +0530

    sss

---
+++ src/AdvancedJavacode/ArraylistIterator.java
@@ -0,0 +1,43 @@
+package AdvancedJavacode;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArraylistIterator {
+
+	public static void main(String[] args) {
+		List<String> list=new ArrayList();
+		
+		list.add("shubham");
+		list.add("ram");
+		list.add("sham");
+		list.add("heyram");
+		list.add("nathuram");
+		list.add("tukaram");
+		/**
+		for(String o:list)
+		{
+			System.out.println(o);
+		}*/
+		/*
+		for (int i = 0; i < list.size(); i++) 
+		{
+			System.out.println(list.get(i));
+		}*/
+		/**
+		Iterator<String> itr=list.iterator();
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}*/
+		
+		
+		List<String> list1=new ArrayList();
+		
+		
+		List<String> list2=new ArrayList();
+
+		
+	}
+
+}
